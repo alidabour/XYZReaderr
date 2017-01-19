51ea3d3a8e3b59e905eb6f0693bdfa1aaaeff930
alidabour <alidabour0@gmail.com>
2017-01-20 00:59:46 +0200

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -0,0 +1,184 @@
+package com.example.xyzreader.ui;
+
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.example.xyzreader.R;
+import com.example.xyzreader.data.ArticleLoader;
+
+/**
+ * A fragment representing a single Article detail screen. This fragment is
+ * either contained in a {@link ArticleListActivity} in two-pane mode (on
+ * tablets) or a {@link ArticleDetailActivity} on handsets.
+ */
+public class ArticleDetailFragment extends Fragment implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "ArticleDetailFragment";
+
+    public static final String ARG_ITEM_ID = "item_id";
+
+    private Cursor mCursor;
+    private long mItemId;
+    private View mRootView;
+    private ImageView mImageView;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private TextView mArticleBody;
+    private FloatingActionButton mShareFAB;
+    private Toolbar mToolbar;
+
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public ArticleDetailFragment() {
+    }
+
+    public static ArticleDetailFragment newInstance(long itemId) {
+        Bundle arguments = new Bundle();
+        arguments.putLong(ARG_ITEM_ID, itemId);
+        ArticleDetailFragment fragment = new ArticleDetailFragment();
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            mItemId = getArguments().getLong(ARG_ITEM_ID);
+        }
+
+        setHasOptionsMenu(true);
+    }
+
+    public ArticleDetailActivity getActivityCast() {
+        return (ArticleDetailActivity) getActivity();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // In support library r8, calling initLoader for a fragment in a FragmentPagerAdapter in
+        // the fragment's onCreate may cause the same LoaderManager to be dealt to multiple
+        // fragments because their mIndex is -1 (haven't been added to the activity yet). Thus,
+        // we do this in onActivityCreated.
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
+        mImageView = (ImageView) mRootView.findViewById(R.id.article_detail_image);
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout)mRootView.findViewById(R.id.collapsing_toolbar);
+        mArticleBody = (TextView)mRootView.findViewById(R.id.article_body);
+        mShareFAB = (FloatingActionButton)mRootView.findViewById(R.id.fabShare);
+        mToolbar = (Toolbar) mRootView.findViewById(R.id.mToolbar);
+        mToolbar.setNavigationIcon(getActivity().getResources().getDrawable(R.drawable.ic_arrow_back));
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+            }
+        });
+
+
+        mShareFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
+                        .setType("text/plain")
+                        .setText("Some sample text")
+                        .getIntent(), getString(R.string.action_share)));
+            }
+        });
+
+        bindViews();
+        return mRootView;
+    }
+
+
+
+
+    private void bindViews() {
+        Log.v("Test","bindViews");
+        if (mRootView == null) {
+            return;
+        }
+        if (mCursor != null) {
+            Log.v("Test","mCursor != null");
+            mCollapsingToolbarLayout.setTitle(mCursor.getString(ArticleLoader.Query.TITLE));
+            mArticleBody.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
+            ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
+                    .get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
+                        @Override
+                        public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
+                            Bitmap bitmap = imageContainer.getBitmap();
+                            if (bitmap != null) {
+                                mImageView.setImageBitmap(imageContainer.getBitmap());
+                                Log.v("Test","Image");
+                          }
+                        }
+
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return ArticleLoader.newInstanceForItemId(getActivity(), mItemId);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        if (!isAdded()) {
+            if (cursor != null) {
+                cursor.close();
+            }
+            return;
+        }
+
+        mCursor = cursor;
+        if (mCursor != null && !mCursor.moveToFirst()) {
+            Log.e(TAG, "Error reading item detail cursor");
+            mCursor.close();
+            mCursor = null;
+        }
+
+        bindViews();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mCursor = null;
+        bindViews();
+    }
+
+
+}
